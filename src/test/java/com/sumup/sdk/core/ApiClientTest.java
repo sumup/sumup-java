467596c09bd917cbe41358d2118d4f1087fe7a09
Matouš Dzivjak <matousdzivjak@gmail.com>
2026-03-11 09:51:07 +0100

    fix(client): send problem+json accept header (#51)

--- src/test/java/com/sumup/sdk/core/ApiClientTest.java
+++ src/test/java/com/sumup/sdk/core/ApiClientTest.java
@@ -93,6 +93,8 @@ final class ApiClientTest {
     assertEquals("java", headers.firstValue("X-Sumup-Runtime").orElse(null));
     assertEquals(
         Runtime.version().toString(), headers.firstValue("X-Sumup-Runtime-Version").orElse(null));
+    assertEquals(
+        "application/problem+json, application/json", headers.firstValue("Accept").orElse(null));
   }
 
   @Test
